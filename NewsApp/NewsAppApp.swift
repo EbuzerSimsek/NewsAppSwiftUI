75a9630d60ffa5fa85ee3740b72206eca15eb25b
Ebuzer07 <Ebuzer6060@gmail.com>
2023-07-24 15:34:39 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ebuzer Şimşek on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
